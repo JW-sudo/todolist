1ddebbde11c11f79909ef9e602b6b30995d13496
JW-sudo <janetwang20161@gmail.com>
2019-10-16 17:39:11 -0700

    Updates

---
+++ precache-manifest.132cfd027ee3ec9723be5498fd4379a5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992867868db79c163f6811c7eb80dde1",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "4703ff8c00014638e47d",
+    "url": "/todolist/static/js/2.c076bf2f.chunk.js"
+  },
+  {
+    "revision": "d26eb8c69285220f30f5",
+    "url": "/todolist/static/js/main.62f7d53e.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
